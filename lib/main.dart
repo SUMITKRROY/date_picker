2c39a9c03f980ed9ed20525591f85dd54601739e
sumit <priencraj32@gmail.com>
2023-07-14 03:43:36 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:time_picker_spinner_pop_up/time_picker_spinner_pop_up.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Time Picker Spinner Pop Up',
+      theme: ThemeData(
+        // primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Time Picker Spinner Pop Up'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: TimePickerSpinnerPopUp(
+              mode: CupertinoDatePickerMode.date,
+              initTime: DateTime.now(),
+              minTime: DateTime.now().subtract(const Duration(days: 2)),
+              maxTime: DateTime.now().add(const Duration(days: 10)),
+              barrierColor: Colors.black12, //Barrier Color when pop up show
+              minuteInterval: 1,
+              padding: const EdgeInsets.fromLTRB(12, 10, 12, 10),
+              cancelText: 'Cancel',
+              confirmText: 'OK',
+              pressType: PressType.singlePress,
+              timeFormat: 'dd/MM/yyyy',
+              // Customize your time widget
+              // timeWidgetBuilder: (dateTime) {},
+              onChange: (dateTime) {
+                // Implement your logic with select dateTime
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
